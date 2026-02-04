ded1db56909a153a09521bd975471d0a0628f4a1
Hoda Elkayal <helkayal169@gmail.com>
2026-02-05 01:21:44 +0200

    update onboarding button to navigate to correct screen

--- lib/features/onboarding/widgets/onboarding_button.dart
+++ lib/features/onboarding/widgets/onboarding_button.dart
@@ -1,5 +1,51 @@
+// import 'package:flutter/material.dart';
+// import 'package:weather_app/features/search/screen/search_screen.dart';
+
+// class OnboardingButton extends StatelessWidget {
+//   const OnboardingButton({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: () {
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(builder: (context) => const SearchScreen()),
+//         );
+//       },
+//       child: Container(
+//         padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(40),
+//           gradient: LinearGradient(
+//             colors: [
+//               Theme.of(context).colorScheme.tertiary,
+//               Theme.of(context).colorScheme.onTertiary,
+//             ],
+//           ),
+//           boxShadow: [
+//             BoxShadow(
+//               color: Theme.of(
+//                 context,
+//               ).colorScheme.primary.withValues(alpha: 0.4),
+//               blurRadius: 5,
+//               offset: const Offset(0, 4),
+//             ),
+//           ],
+//         ),
+//         child: Text(
+//           "let's start",
+//           style: Theme.of(context).textTheme.displayMedium,
+//           textAlign: TextAlign.center,
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:weather_app/core/helpers/local_storage_helper.dart';
 import 'package:weather_app/features/search/screen/search_screen.dart';
+import 'package:weather_app/features/weather/screens/weather_setails.dart';
 
 class OnboardingButton extends StatelessWidget {
   const OnboardingButton({super.key});
@@ -8,13 +54,22 @@ class OnboardingButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const SearchScreen()),
-        );
+        final lastCity = LocalStorageHelper.getLastCity();
+
+        if (lastCity != null && lastCity.isNotEmpty) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => WeatherDetails(city: lastCity)),
+          );
+        } else {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const SearchScreen()),
+          );
+        }
       },
       child: Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(40),
           gradient: LinearGradient(
